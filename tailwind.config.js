cb3698f0ef79a1558fd4c1b46182ee094b1fcd05
Willy Fajar Ramadhan <willyfr88@gmail.com>
2022-05-28 13:14:00 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'light-cyan': 'var(--light-cyan)',
+        'neon-green': 'var(--neon-green)',
+        'grayish-blue': 'var(--grayish-blue)',
+        'dark-grayish-blue': 'var(--dark-grayish-blue)',
+        'dark-blue': 'var(--dark-blue)',
+      },
+      fontFamily: {
+        'manrope': ['Manrope', 'Serif'],
+      },
+    },
+  },
+  plugins: [],
+}
